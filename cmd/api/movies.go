f8a9f5776f3279a6ad96cba6fb28f3ca25bafbbc
mavro <mrojasb2000@gmail.com>
2021-05-11 21:37:20 -0400

    Refactor movies actions

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -23,4 +24,19 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	// stored in the request context. We can use the ParamsFromContext() function to
 	// retrieve a slice containing these parameter names and values.
 	params := httprouter.ParamsFromContext(r.Context())
+
+	// We can then use the ByName() method to get the value of the "id" parameter from
+	// the slice. In our project all movies will have a unique positive integer ID, but
+	// the value returned by ByName() is always a string. So we try to convert it to a
+	// base 10 interger (with a bit size of 64). If the parameter couldn't be converted,
+	// or is less than 1, we known the ID is invalid so we use the http.NotFound()
+	// function to return a 404 Not Found response.
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Otherwise, interpolate the movie ID in a placeholder response.
+	fmt.Fprintf(w, "show the details of movie %d\n", id)
 }
