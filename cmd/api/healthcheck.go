06dd1ce0c3161ebf409a0526049adc2b47724ad0
mavro <mrojasb2000@gmail.com>
2021-05-13 18:19:05 -0400

    Added comment README

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,7 +5,8 @@ import (
 	"net/http"
 )
 
-// Declare a handler which writes a plain-text response with information about the application status, operating environment and version.
+// Declare a handler which writes a plain-text response with information about
+// the application status, operating environment and version.
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	version := "0.0.1"
