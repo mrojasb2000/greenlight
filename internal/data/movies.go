5bb706e8e67616821b1efa0fc266382eaf0d70ab
mavro <mrojasb2000@gmail.com>
2021-05-14 12:44:15 -0400

    Added keys tags JSON

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,12 +2,14 @@ package data
 
 import "time"
 
+// Annotate the Movie struct with struct tags to control how the keys appear in the
+// JSON-encoded output.
 type Movie struct {
-	ID       int64     // Unique integer ID for the movie
-	CreateAt time.Time // Timestamp for when the movie is added to our database
-	Title    string    // Movie title
-	Year     int32     // Movie release year
-	Runtime  int32     // Movie runtime (in minutes)
-	Genres   []string  // Slice of geners for the movie (romace, comedy, etc.)
-	Version  int32     // The version number start 1 and will be incremented each time the movie information is updated
+	ID       int64     `json:"id"`         // Unique integer ID for the movie
+	CreateAt time.Time `json:"created_at"` // Timestamp for when the movie is added to our database
+	Title    string    `json:"title"`      // Movie title
+	Year     int32     `json:"year"`       // Movie release year
+	Runtime  int32     `json:"runtime"`    // Movie runtime (in minutes)
+	Genres   []string  `json:"benres"`     // Slice of geners for the movie (romace, comedy, etc.)
+	Version  int32     `json:"version"`    // The version number start 1 and will be incremented each time the movie information is updated
 }
