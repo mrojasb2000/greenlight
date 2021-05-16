6b963e804c15073088240095da2e7f09b4792fe8
mavro <mrojasb2000@gmail.com>
2021-05-16 17:30:17 -0400

    Refactor change format int to string

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -5,11 +5,11 @@ import "time"
 // Annotate the Movie struct with struct tags to control how the keys appear in the
 // JSON-encoded output.
 type Movie struct {
-	ID       int64     `json:"id"`         // Unique integer ID for the movie
-	CreateAt time.Time `json:"created_at"` // Timestamp for when the movie is added to our database
-	Title    string    `json:"title"`      // Movie title
-	Year     int32     `json:"year"`       // Movie release year
-	Runtime  int32     `json:"runtime"`    // Movie runtime (in minutes)
-	Genres   []string  `json:"benres"`     // Slice of geners for the movie (romace, comedy, etc.)
-	Version  int32     `json:"version"`    // The version number start 1 and will be incremented each time the movie information is updated
+	ID       int64     `json:"id"`                       // Unique integer ID for the movie
+	CreateAt time.Time `json:"-"`                        // Timestamp for when the movie is added to our database
+	Title    string    `json:"title"`                    // Movie title
+	Year     int32     `json:"year,omitempty"`           // Movie release year
+	Runtime  int32     `json:"runtime,omitempty,string"` // Movie runtime (in minutes)
+	Genres   []string  `json:"genres,omitempty"`         // Slice of geners for the movie (romace, comedy, etc.)
+	Version  int32     `json:"version"`                  // The version number start 1 and will be incremented each time the movie information is updated
 }
