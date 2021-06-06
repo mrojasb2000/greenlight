ad16102f92742e048da7c7e4565e27091857a8dd
mavro <mrojasb2000@gmail.com>
2021-06-06 14:14:01 -0400

    Refactor concurrent update movies

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -70,3 +70,10 @@ func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.
 	message := fmt.Sprintf("the %s method is not supported fr this resource", r.Method)
 	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
 }
+
+// The editConflictResponse() method will be used to send a 409 Conflict
+// status code and JSON response to the client.
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+	message := "unable to update the record due to an edit conflict, please try again"
+	app.errorResponse(w, r, http.StatusConflict, message)
+}
